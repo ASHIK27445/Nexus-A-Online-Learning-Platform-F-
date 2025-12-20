1205dc01c4fa44faa44f319f8d3951572db7a6af
ASHIK27445 <mdashikulislama57@gmail.com>
2025-12-20 14:01:33 +0600

    Setting up the routes

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter } from "react-router"
+import HomePage from "../Home/HomePage";
+import App from "../App";
+import Demo from "../Demo"
+import Login from "../components/Authentication/Login";
+import Register from "../components/Authentication/Register";
+import PrivateRouter from "../components/Authentication/PrivateRouter";
+import Dashboard from "../components/Dashboard/Dashboard";
+import AddCourse from "../components/course/AddCourse";
+import DetailsCourse from "../components/course/DetailsCourse";
+import MyCourses from "../components/course/MyCourse";
+import AllCourses from "../components/course/AllCourses";
+import UpdateCourse from "../components/course/UpdateCourse";
+import EnrolledCourses from "../components/course/EnrolledCourses";
+
+export const router = createBrowserRouter([
+    {
+        path:'/', Component: App ,
+        children: [
+            {
+                index: true, Component: HomePage
+            },
+            {
+                path:"/login", Component: Login
+            },
+            {
+                path:'/register', Component: Register
+            },
+            {
+                path: '/dashboard', element: <PrivateRouter><Dashboard></Dashboard></PrivateRouter>
+            },
+            {
+                path: '/dashboard/addCourse', element: <PrivateRouter><AddCourse></AddCourse></PrivateRouter>
+            },
+            {
+                path: '/viewDetails/:id', element: <PrivateRouter><DetailsCourse></DetailsCourse></PrivateRouter>
+            },
+            {
+                path: '/myCourses', element: <PrivateRouter><MyCourses></MyCourses></PrivateRouter>
+            },
+            {
+                path:'/allCourses', element: <AllCourses></AllCourses>
+            },
+            {
+                path:'/updateCourse/:id', element: <PrivateRouter><UpdateCourse></UpdateCourse></PrivateRouter>
+            },
+            {
+                path:'/myEnrollCourse', element:<PrivateRouter><EnrolledCourses></EnrolledCourses></PrivateRouter> 
+            }
+        ]
+    }
+])
